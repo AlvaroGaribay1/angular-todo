f10a4ff27c4e0305bf5d322a8c1d0c4644979949
AlvaroGaribay1 <garciagaribayalvaro@gmail.com>
2025-01-10 16:00:59 -0600

    CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
-import { TODO_DATA } from '../assets/todo';
 import { NTodo } from './models/todo.model';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './pages/header/header.component';
+import { ApiService } from './services/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
+  imports: [
+    RouterOutlet,
     TodoComponent,
-    CommonModule
+    CommonModule,
+    HeaderComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  todos: NTodo.TodosResponse = { totalRecords: 0, data: []};
 
-  todoData = TODO_DATA;
+
+  constructor(
+    private readonly ApiService: ApiService
+  ) {}
+
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
+  private getTodos() {
+    this.ApiService.get<NTodo.TodosResponse>()
+    .subscribe(val => this.todos = val);
+  }
 
   getTodoInfo(val: NTodo.TodoData) {
     console.log(val);
   }
 
-  trackByFunction(index: number, item: NTodo.TodoData) {
-    return index;
+  updateTodo(item: NTodo.TodoData) {
+    // this.ApiService.put(item, item.id)
+    // .subscribe(console.log);
+    this.ApiService.patch({ description : item.description }, item.id)
+    .subscribe(console.log);
   }
 
-  orderData() {
-    //Ordena por prioridad
-    this.todoData.sort((a, b) => a.priority - b.priority);
+  agregarTodo() {
+    this.ApiService.post({
+      "title": "Leer documentación técnica",
+      "description": "Investigar y leer la documentación de una nueva tecnología o herramienta relevante para el proyecto.",
+      "status": "Por hacer",
+      "priority": 3,
+      "hidden": false,
+      "id": 1,
+      "deadLine": "2024-04-07T03:25:54.898Z",
+      "color": {
+        "status": "#ed4040",
+        "priority": "#68db68"
+      },
+      "class": {
+        "status": "to-do",
+        "priority": "low"
+      },
+      "progress": 0.2
+    })
+    .subscribe(() => this.getTodos());
   }
+
+  deleteTodo(item: NTodo.TodoData) {
+    this.ApiService.delete<NTodo.TodosResponse>(item.id)
+    .subscribe(todos => this.todos = todos);
+  }
+
 }
