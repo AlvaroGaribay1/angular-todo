834d16df20b5ed1323796c08e4355c8b9852ad54
AlvaroGaribay1 <garciagaribayalvaro@gmail.com>
2025-01-10 13:52:19 -0600

    10/01/2024
    Implementación de directivas Standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
 import { TODO_DATA } from '../assets/todo';
 import { NTodo } from './models/todo.model';
 import { CommonModule } from '@angular/common';
+import { HighlightedDirective } from './directives/highlighted.directive';
+import { NgxUnlessDirective } from './directives/ngx-unless.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, 
     TodoComponent,
-    CommonModule
+    CommonModule,
+    HighlightedDirective,
+    NgxUnlessDirective
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  todoData = TODO_DATA;
+  todoData = TODO_DATA[0];
+
+  constructor() {}
+  
+  ngAfterViewInit(): void {
+    // console.log(this.highlightedDirective);
+  }
+
+  @ViewChild('todoRef', {read: HighlightedDirective}) highlightedDirective!: HighlightedDirective;
 
   getTodoInfo(val: NTodo.TodoData) {
     console.log(val);
@@ -27,8 +39,14 @@ export class AppComponent {
     return index;
   }
 
-  orderData() {
-    //Ordena por prioridad
-    this.todoData.sort((a, b) => a.priority - b.priority);
+  // orderData() {
+  //   //Ordena por prioridad
+  //   this.todoData.sort((a, b) => a.priority - b.priority);
+  // }
+
+
+  onToggleHighlighted(highlighted: boolean) {
+    console.log(highlighted);
   }
+
 }
